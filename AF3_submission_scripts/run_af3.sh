9f810a9b5db58b55de959c812e3d0a9d4756d39d
Richard Birkinshaw <birkinshaw.r@wehi.edu.au>
2025-03-20 12:43:44 +1100

    add generalised submissionscript

---
+++ AF3_submission_scripts/run_af3.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+read -e -p 'Input json file:' jsonfile
+if [ ! -f "$pdbfile" ] 
+then
+    echo -e "\nERROR: File (${jsonfile}) does not exist. Run again." 
+    exit 1
+fi
+modelname="${jsonfile%.*}"
+
+export jsonfile modelname
+features=$(sbatch --parsable --export=jsonfile,modelname features.sh)
+sbatch --export=jsonfile,modelname --dependency=afterok:$features inference.sh
